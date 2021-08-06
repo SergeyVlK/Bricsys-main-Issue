a4416ee85d1496b87bee711334d9e7078e6f0e17
Sergey K <kanaev.sergey.v@yandex.ru>
2021-08-06 23:05:11 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+mport pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mysql.connector
+
+import config
+
+db = mysql.connector.connect(
+    host=config.host,              # your host, usually localhost
+    user=config.user,            # your username
+    password=config.password,        # your password
+    database=config.database     # name of the data base
+)
+
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM testresults WHERE testid=2 ORDER BY create_time;")
+
+# Put it all to a data frame
+sql_data = pd.DataFrame(cur.fetchall())
+sql_data.columns = cur.column_names
+
+# Close the session
+db.close()

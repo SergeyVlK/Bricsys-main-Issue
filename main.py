8adb193235ab25d8af837d58ea05b3c64a6505ea
Sergey K <kanaev.sergey.v@yandex.ru>
2021-08-07 22:29:58 +0700

    ploting and fft are working

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import mysql.connector
 import config
 
 db = mysql.connector.connect(
-    host=config.host,              # your host, usually localhost
-    user=config.user,            # your username
-    password=config.password,        # your password
-    database=config.database     # name of the data base
+    host=config.host,
+    user=config.user,
+    password=config.password,
+    database=config.database
 )
 
 
@@ -23,3 +23,21 @@ sql_data.columns = cur.column_names
 
 # Close the session
 db.close()
+
+plt.plot(sql_data["buildid"], sql_data["duration"], label='Original')
+
+spectr_sin = np.fft.rfft(sql_data["duration"])
+
+# print(spectr_sin)
+spectr_sin[:200] = 0
+spectr_sin[260:] = 0
+Y = np.fft.irfft(spectr_sin)
+# # print("len(x) = " + str(len(sql_data)))
+# # print("len(Y) = " + str(len(Y)))
+if(len(Y) != len(sql_data)):
+    plt.plot(sql_data["buildid"][:-1], Y, label='Transformed')
+else:
+    plt.plot(sql_data["buildid"], Y, label='Transformed')
+plt.legend(loc=0)
+plt.grid(True)
+plt.show()

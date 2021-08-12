c5091f76114138f27fb88fa3c4663afdaa6c1de8
Sergey K <kanaev.sergey.v@yandex.ru>
2021-08-13 00:01:02 +0700

    For 4 tests, clustering works approximately normally

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def outlier_detector(df):
 
 
 # test_number = int(input("Enter number of test: "))
-test_number = 4
+test_number =4
 
 db = mysql.connector.connect(
     host=config.host,
@@ -59,8 +59,10 @@ data, coef = normalisation_axis(sql_data)
 clusters = outlier_detector(data)
 
 
+
 data_without_outlier = data.drop(data.index[[i for i, e in enumerate(clusters) if e == -1]])
-clusters_without_outlier = [value for value in clusters if value != -1]
+clusters_without_outlier = np.delete(clusters, np.where(clusters == -1))
+data_without_outlier= data_without_outlier.reset_index(drop=True)
 
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
@@ -89,6 +91,7 @@ plt.show()
 
 if(test_number == 4):
     data_without_outlier  = data_without_outlier[(data_without_outlier.duration != 4.) & (data_without_outlier.duration != 6.)]
+    data_without_outlier = data_without_outlier.reset_index(drop=True)
 
 
     clusters_2_iter = DBSCAN(
@@ -106,8 +109,84 @@ if(test_number == 4):
       colorbar = False
     )
     plt.show()
+    # print("длина датафрейма до удаления выбросов: " + str(len(data_without_outlier)))
+    # print("длина массива кластеров до удаления выбросов: " + str(len(clusters_2_iter)))
+
+    data_without_outlier = data_without_outlier.drop(data_without_outlier.index[[i for i, e in enumerate(clusters_2_iter) if e == -1]])
+    clusters_without_outlier = np.delete(clusters_2_iter, np.where(clusters_2_iter == -1))
+    data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+    # print("длина датафрейма: " + str(len(data_without_outlier)))
+    # print("длина массива кластеров: " + str(len(clusters_without_outlier)))
+
+
+if(test_number == 3):
+    data_without_outlier = data_without_outlier[data_without_outlier.duration != 1.]
+    data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+    clusters_2_iter = DBSCAN(
+        eps=0.3,
+        metric="euclidean",
+        min_samples=10,
+        n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
+
+    cmap = cm.get_cmap('Accent')
+    data_without_outlier.plot.scatter(
+        x="buildid",
+        y="duration",
+        c=clusters_2_iter,
+        cmap=cmap,
+        colorbar=False
+    )
+    plt.show()
+    clusters_without_outlier = clusters_2_iter[:]
+
 
 # print(list(clusters_2_iter))
+
+buildid_points = []
+duration_persents = []
+prev_index = 0
+for index in range(1, len(clusters_without_outlier)):
+    if(clusters_without_outlier[index] != clusters_without_outlier[prev_index]):
+        buildid_points.append(int(data_without_outlier.at[index, "buildid"] / coef))
+        duration_persents.append((data_without_outlier.at[index, "duration"] - data_without_outlier.at[prev_index, "duration"]) /
+                                 (data_without_outlier.at[prev_index, "duration"]) * 100)
+
+        # print("prev_index = ", prev_index)
+        # print("prev_clas = ", clusters_without_outlier[prev_index])
+        # print("prev = ", data_without_outlier.at[prev_index, "duration"])
+        # print("index = ", index)
+        # print("prev_clas = ", clusters_without_outlier[index])
+        # print("current = ", data_without_outlier.at[index, "duration"])
+        # print()
+
+    prev_index = index
+
+# print("длина датафрейма: " + str(len(data_without_outlier)))
+# print("длина массива кластеров: " + str(len(clusters_without_outlier)))
+print((set(clusters_without_outlier)))
+cmap = cm.get_cmap('Accent')
+data_without_outlier.plot.scatter(
+        x="buildid",
+        y="duration",
+        c=clusters_without_outlier,
+        cmap=cmap,
+        colorbar=False
+    )
+plt.plot()
+
+print("Айди измениний: ")
+print(buildid_points)
+print("На сколько процентов изменилось: ")
+print(duration_persents)
+# print("Кластеры:" )
+# print(clusters_without_outlier)
+# print(data_without_outlier.head(40))
+# print("Индекс первого изменения:")
+# print(np.where(clusters_without_outlier == 1))
+
+
 
 
 

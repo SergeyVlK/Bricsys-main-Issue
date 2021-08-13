6a360aedaf1c92fbe959724b23249cadc91a4f0a
Sergey K <kanaev.sergey.v@yandex.ru>
2021-08-13 16:40:29 +0700

    searching points abd writing to database are working

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def normalisation_axis(df):
 
     data = df[["buildid", "duration"]]
     data["buildid"] *= coef
+    # data['buildid'] = data['buildid'].apply(lambda x: x * coef)
+    # data.loc[:, 'buildid'] *= coef
     return data, coef
 
 def outlier_detector(df):
@@ -31,160 +33,139 @@ def outlier_detector(df):
     clusters = outlier_detection.fit_predict(df)
     return clusters
 
+def outlier_filter(df, clusters_ar):
+    df_wo = df.drop(df.index[[i for i, e in enumerate(clusters_ar) if e == -1]])
+    cl_wo = np.delete(clusters_ar, np.where(clusters_ar == -1))
+    df_wo = df_wo.reset_index(drop=True)
+    return df_wo, cl_wo
 
-# test_number = int(input("Enter number of test: "))
-test_number =4
+def plot_source_data(df_source, cl_source, df_wo, cl_wo):
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.set_title("Исходное множество")
+    cmap = cm.get_cmap('Accent')
+    df_source.plot.scatter(
+        x="buildid",
+        y="duration",
+        c=cl_source,
+        cmap=cmap,
+        colorbar=False,
+        ax=ax1)
+
+    ax2.set_title("Множество без выбросов")
+    cmap = cm.get_cmap('Accent')
+    df_wo.plot.scatter(
+        x="buildid",
+        y="duration",
+        c=cl_wo,
+        cmap=cmap,
+        colorbar=False,
+        ax=ax2)
+    fig.tight_layout()
+    plt.show()
+
+def plot_clastered_data(df, cl):
+    cmap = cm.get_cmap('Accent')
+    df.plot.scatter(
+        x="buildid",
+        y="duration",
+        c=cl,
+        cmap=cmap,
+        colorbar=False
+    )
+    plt.plot()
+
+def search_points(df, cl):
+    buildid_points = []
+    duration_persents = []
+    prev_index = 0
+    for index in range(1, len(cl)):
+        if (cl[index] != cl[prev_index]):
+            buildid_points.append(int(df.at[index, "buildid"] / coef))
+            duration_persents.append(
+                (df.at[index, "duration"] - df.at[prev_index, "duration"]) /
+                (df.at[prev_index, "duration"]) * 100)
+        prev_index = index
+    return buildid_points, duration_persents
+
+
 
 db = mysql.connector.connect(
-    host=config.host,
-    user=config.user,
-    password=config.password,
-    database=config.database
-)
-
-
+        host=config.host,
+        user=config.user,
+        password=config.password,
+        database=config.database
+    )
 cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS points("
+            "buildid INT NOT NULL, "
+            "testid INT NOT NULL, "
+            "slowdownOrSpeedup FLOAT(5,2) NOT NULL,"
+            " PRIMARY KEY (buildid, testid),"
+            "CONSTRAINT fk_points_testresults "
+            "FOREIGN KEY (testid,buildid) REFERENCES testresults(testid,buildid) "
+            "ON UPDATE CASCADE "
+            "ON DELETE CASCADE, "
+            "CONSTRAINT fk_points_testnames FOREIGN KEY (testid) REFERENCES testnames(id) "
+            "ON UPDATE CASCADE "
+            "ON DELETE CASCADE);")
+db.commit()
+for test_number in range(1,5):
 
-cur.execute("SELECT * FROM testresults WHERE testid=" + str(test_number) + " ORDER BY create_time;")
 
-# Put it all to a data frame
-sql_data = pd.DataFrame(cur.fetchall())
-sql_data.columns = cur.column_names
+    cur.execute("SELECT * FROM testresults WHERE testid=" + str(test_number) + " ORDER BY create_time;")
 
-# Close the session
+    # Put it all to a data frame
+    sql_data = pd.DataFrame(cur.fetchall())
+    sql_data.columns = cur.column_names
+
+    # Close the session
+    # db.close()
+
+    data, coef = normalisation_axis(sql_data)
+    clusters = outlier_detector(data)
+
+    data_without_outlier, clusters_without_outlier = outlier_filter(data, clusters)
+    # plot_source_data(data, clusters, data_without_outlier, clusters_without_outlier)
+
+
+    if(test_number == 4):
+        data_without_outlier  = data_without_outlier[(data_without_outlier.duration != 4.) & (data_without_outlier.duration != 6.)]
+        data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+        clusters_2_iter = DBSCAN(
+          eps = 0.1,
+          metric="euclidean",
+          min_samples = 10,
+          n_jobs = -1).fit_predict(data_without_outlier[["duration"]])
+
+        # plot_clastered_data(data_without_outlier, clusters_2_iter)
+        data_without_outlier, clusters_without_outlier = outlier_filter(data_without_outlier, clusters_2_iter)
+
+
+    if(test_number == 3):
+        data_without_outlier = data_without_outlier[data_without_outlier.duration != 1.]
+        data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+        clusters_2_iter = DBSCAN(
+            eps=0.3,
+            metric="euclidean",
+            min_samples=10,
+            n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
+
+        # plot_clastered_data(data_without_outlier, clusters_2_iter)
+        clusters_without_outlier = clusters_2_iter
+
+    buildid_points,duration_persents=search_points(data_without_outlier, clusters_without_outlier)
+
+    count = 0
+    while(count < len(buildid_points)):
+        cur.execute("INSERT INTO points(testid, buildid, slowdownOrSpeedup) VALUES({t_id},{b_id},{value});".format(t_id=test_number, b_id=buildid_points[count], value=duration_persents[count]))
+        count += 1
+    db.commit()
+
+
 db.close()
 
-
-data, coef = normalisation_axis(sql_data)
-clusters = outlier_detector(data)
-
-
-
-data_without_outlier = data.drop(data.index[[i for i, e in enumerate(clusters) if e == -1]])
-clusters_without_outlier = np.delete(clusters, np.where(clusters == -1))
-data_without_outlier= data_without_outlier.reset_index(drop=True)
-
-
-fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.set_title("Исходное множество")
-cmap = cm.get_cmap('Accent')
-sql_data.plot.scatter(
-  x = "buildid",
-  y = "duration",
-  c = clusters,
-  cmap = cmap,
-  colorbar = False,
-  ax= ax1)
-
-ax2.set_title("Множество без выбросов")
-cmap = cm.get_cmap('Accent')
-data_without_outlier.plot.scatter(
-  x = "buildid",
-  y = "duration",
-  c = clusters_without_outlier,
-  cmap = cmap,
-  colorbar = False,
-  ax= ax2)
-fig.tight_layout()
-plt.show()
-
-
-if(test_number == 4):
-    data_without_outlier  = data_without_outlier[(data_without_outlier.duration != 4.) & (data_without_outlier.duration != 6.)]
-    data_without_outlier = data_without_outlier.reset_index(drop=True)
-
-
-    clusters_2_iter = DBSCAN(
-      eps = 0.1,
-      metric="euclidean",
-      min_samples = 10,
-      n_jobs = -1).fit_predict(data_without_outlier[["duration"]])
-
-    cmap = cm.get_cmap('Accent')
-    data_without_outlier.plot.scatter(
-      x = "buildid",
-      y = "duration",
-      c = clusters_2_iter,
-      cmap = cmap,
-      colorbar = False
-    )
-    plt.show()
-    # print("длина датафрейма до удаления выбросов: " + str(len(data_without_outlier)))
-    # print("длина массива кластеров до удаления выбросов: " + str(len(clusters_2_iter)))
-
-    data_without_outlier = data_without_outlier.drop(data_without_outlier.index[[i for i, e in enumerate(clusters_2_iter) if e == -1]])
-    clusters_without_outlier = np.delete(clusters_2_iter, np.where(clusters_2_iter == -1))
-    data_without_outlier = data_without_outlier.reset_index(drop=True)
-
-    # print("длина датафрейма: " + str(len(data_without_outlier)))
-    # print("длина массива кластеров: " + str(len(clusters_without_outlier)))
-
-
-if(test_number == 3):
-    data_without_outlier = data_without_outlier[data_without_outlier.duration != 1.]
-    data_without_outlier = data_without_outlier.reset_index(drop=True)
-
-    clusters_2_iter = DBSCAN(
-        eps=0.3,
-        metric="euclidean",
-        min_samples=10,
-        n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
-
-    cmap = cm.get_cmap('Accent')
-    data_without_outlier.plot.scatter(
-        x="buildid",
-        y="duration",
-        c=clusters_2_iter,
-        cmap=cmap,
-        colorbar=False
-    )
-    plt.show()
-    clusters_without_outlier = clusters_2_iter[:]
-
-
-# print(list(clusters_2_iter))
-
-buildid_points = []
-duration_persents = []
-prev_index = 0
-for index in range(1, len(clusters_without_outlier)):
-    if(clusters_without_outlier[index] != clusters_without_outlier[prev_index]):
-        buildid_points.append(int(data_without_outlier.at[index, "buildid"] / coef))
-        duration_persents.append((data_without_outlier.at[index, "duration"] - data_without_outlier.at[prev_index, "duration"]) /
-                                 (data_without_outlier.at[prev_index, "duration"]) * 100)
-
-        # print("prev_index = ", prev_index)
-        # print("prev_clas = ", clusters_without_outlier[prev_index])
-        # print("prev = ", data_without_outlier.at[prev_index, "duration"])
-        # print("index = ", index)
-        # print("prev_clas = ", clusters_without_outlier[index])
-        # print("current = ", data_without_outlier.at[index, "duration"])
-        # print()
-
-    prev_index = index
-
-# print("длина датафрейма: " + str(len(data_without_outlier)))
-# print("длина массива кластеров: " + str(len(clusters_without_outlier)))
-print((set(clusters_without_outlier)))
-cmap = cm.get_cmap('Accent')
-data_without_outlier.plot.scatter(
-        x="buildid",
-        y="duration",
-        c=clusters_without_outlier,
-        cmap=cmap,
-        colorbar=False
-    )
-plt.plot()
-
-print("Айди измениний: ")
-print(buildid_points)
-print("На сколько процентов изменилось: ")
-print(duration_persents)
-# print("Кластеры:" )
-# print(clusters_without_outlier)
-# print(data_without_outlier.head(40))
-# print("Индекс первого изменения:")
-# print(np.where(clusters_without_outlier == 1))
 
 
 

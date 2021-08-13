f38dc918d92b27c6e5765ef1a03c60bdd44d2fb1
Sergey K <kanaev.sergey.v@yandex.ru>
2021-08-13 20:38:31 +0700

    added simple plot. line- source data, vertical red lines - points of duration changed

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def plot_clastered_data(df, cl):
     )
     plt.plot()
 
-def search_points(df, cl):
+def search_points(df, cl, coef):
     buildid_points = []
     duration_persents = []
     prev_index = 0
@@ -87,84 +87,106 @@ def search_points(df, cl):
         prev_index = index
     return buildid_points, duration_persents
 
-
-
-db = mysql.connector.connect(
+def main():
+    db = mysql.connector.connect(
         host=config.host,
         user=config.user,
         password=config.password,
         database=config.database
     )
-cur = db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS points("
-            "buildid INT NOT NULL, "
-            "testid INT NOT NULL, "
-            "slowdownOrSpeedup FLOAT(5,2) NOT NULL,"
-            " PRIMARY KEY (buildid, testid),"
-            "CONSTRAINT fk_points_testresults "
-            "FOREIGN KEY (testid,buildid) REFERENCES testresults(testid,buildid) "
-            "ON UPDATE CASCADE "
-            "ON DELETE CASCADE, "
-            "CONSTRAINT fk_points_testnames FOREIGN KEY (testid) REFERENCES testnames(id) "
-            "ON UPDATE CASCADE "
-            "ON DELETE CASCADE);")
-db.commit()
-for test_number in range(1,5):
-
-
-    cur.execute("SELECT * FROM testresults WHERE testid=" + str(test_number) + " ORDER BY create_time;")
-
-    # Put it all to a data frame
-    sql_data = pd.DataFrame(cur.fetchall())
-    sql_data.columns = cur.column_names
-
-    # Close the session
-    # db.close()
-
-    data, coef = normalisation_axis(sql_data)
-    clusters = outlier_detector(data)
-
-    data_without_outlier, clusters_without_outlier = outlier_filter(data, clusters)
-    # plot_source_data(data, clusters, data_without_outlier, clusters_without_outlier)
-
-
-    if(test_number == 4):
-        data_without_outlier  = data_without_outlier[(data_without_outlier.duration != 4.) & (data_without_outlier.duration != 6.)]
-        data_without_outlier = data_without_outlier.reset_index(drop=True)
-
-        clusters_2_iter = DBSCAN(
-          eps = 0.1,
-          metric="euclidean",
-          min_samples = 10,
-          n_jobs = -1).fit_predict(data_without_outlier[["duration"]])
-
-        # plot_clastered_data(data_without_outlier, clusters_2_iter)
-        data_without_outlier, clusters_without_outlier = outlier_filter(data_without_outlier, clusters_2_iter)
-
-
-    if(test_number == 3):
-        data_without_outlier = data_without_outlier[data_without_outlier.duration != 1.]
-        data_without_outlier = data_without_outlier.reset_index(drop=True)
-
-        clusters_2_iter = DBSCAN(
-            eps=0.3,
-            metric="euclidean",
-            min_samples=10,
-            n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
-
-        # plot_clastered_data(data_without_outlier, clusters_2_iter)
-        clusters_without_outlier = clusters_2_iter
-
-    buildid_points,duration_persents=search_points(data_without_outlier, clusters_without_outlier)
-
-    count = 0
-    while(count < len(buildid_points)):
-        cur.execute("INSERT INTO points(testid, buildid, slowdownOrSpeedup) VALUES({t_id},{b_id},{value});".format(t_id=test_number, b_id=buildid_points[count], value=duration_persents[count]))
-        count += 1
+    cur = db.cursor()
+    cur.execute("DROP TABLE IF EXISTS points;")
+    cur.execute("CREATE TABLE IF NOT EXISTS points("
+                "buildid INT NOT NULL, "
+                "testid INT NOT NULL, "
+                "slowdownOrSpeedup FLOAT(5,2) NOT NULL,"
+                " PRIMARY KEY (buildid, testid),"
+                "CONSTRAINT fk_points_testresults "
+                "FOREIGN KEY (testid,buildid) REFERENCES testresults(testid,buildid) "
+                "ON UPDATE CASCADE "
+                "ON DELETE CASCADE, "
+                "CONSTRAINT fk_points_testnames FOREIGN KEY (testid) REFERENCES testnames(id) "
+                "ON UPDATE CASCADE "
+                "ON DELETE CASCADE);")
     db.commit()
+    for test_number in range(1, 5):
+
+        cur.execute("SELECT * FROM testresults WHERE testid=" + str(test_number) + " ORDER BY create_time;")
+
+        sql_data = pd.DataFrame(cur.fetchall())
+        sql_data.columns = cur.column_names
+
+        data, coef = normalisation_axis(sql_data)
+        clusters = outlier_detector(data)
+
+        data_without_outlier, clusters_without_outlier = outlier_filter(data, clusters)
+        # plot_source_data(data, clusters, data_without_outlier, clusters_without_outlier)
+
+        if (test_number == 4):
+            data_without_outlier = data_without_outlier[
+                (data_without_outlier.duration != 4.) & (data_without_outlier.duration != 6.)]
+            data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+            clusters_2_iter = DBSCAN(
+                eps=0.1,
+                metric="euclidean",
+                min_samples=10,
+                n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
+
+            # plot_clastered_data(data_without_outlier, clusters_2_iter)
+            data_without_outlier, clusters_without_outlier = outlier_filter(data_without_outlier, clusters_2_iter)
+
+        if (test_number == 3):
+            data_without_outlier = data_without_outlier[data_without_outlier.duration != 1.]
+            data_without_outlier = data_without_outlier.reset_index(drop=True)
+
+            clusters_2_iter = DBSCAN(
+                eps=0.3,
+                metric="euclidean",
+                min_samples=10,
+                n_jobs=-1).fit_predict(data_without_outlier[["duration"]])
+
+            # plot_clastered_data(data_without_outlier, clusters_2_iter)
+            clusters_without_outlier = clusters_2_iter
+
+        buildid_points, duration_persents = search_points(data_without_outlier, clusters_without_outlier, coef)
+
+        count = 0
+        while (count < len(buildid_points)):
+            cur.execute("INSERT INTO points(testid, buildid, slowdownOrSpeedup) VALUES({t_id},{b_id},{value});".format(
+                t_id=test_number, b_id=buildid_points[count], value=duration_persents[count]))
+            count += 1
+        db.commit()
+
+    db.close()
+
+def visualization(testid):
+    db = mysql.connector.connect(
+        host=config.host,
+        user=config.user,
+        password=config.password,
+        database=config.database
+    )
+    cur = db.cursor()
+    cur.execute("SELECT buildid, duration FROM testresults WHERE testid=" + str(testid) + " ORDER BY create_time;")
+    data = pd.DataFrame(cur.fetchall())
+    data.columns = cur.column_names
+
+    cur.execute("SELECT * FROM points WHERE testid=" + str(testid) + " ORDER BY testid, buildid;")
+    rows = cur.fetchall()
+    if(len(rows)>0):
+        points = pd.DataFrame(rows)
+        points.columns = cur.column_names
+        plt.vlines(points[["buildid"]], 0, data[["duration"]].max(), color="r", linewidth=5, linestyle="--")
+
+    db.close()
+
+    plt.plot(data[["buildid"]],data[["duration"]])
+    plt.show()
 
 
-db.close()
+main()
+visualization(3)
 
 
 
